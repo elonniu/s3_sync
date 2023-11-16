e46e5820b85c15a5cc0d2078d8bf1448f8c4828f
Jingyi <elonniu@amazon.com>
2023-11-16 19:25:20 +0800

    rename file

--- sync_file.sh
+++ sync_file.sh
@@ -33,7 +33,7 @@ echo "Local ETag: $local_etag"
 # Compare ETags to check if the files are the same
 if [ "$s3_etag" == "$local_etag" ]; then
     echo "S3 file and local file are the same. No need to download."
-    return 0
+    return
 fi
 
 aws s3 cp "s3://$s3_bucket/$s3_object_key" "$local_file"
