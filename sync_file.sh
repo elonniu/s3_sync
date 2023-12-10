4cbbdd78333d0b7e2b605e9c76b13ff279647551
Jingyi <elonniu@amazon.com>
2023-12-10 23:47:56 +0800

    improved shell tip

--- sync_file.sh
+++ sync_file.sh
@@ -8,14 +8,15 @@ echo "-------------- sync_file.sh --------------"
 s3_bucket="elonniu"
 s3_object_key="files/colors.txt"
 
+s3_file="s3://$s3_bucket/$s3_object_key"
 # Define local file path
 local_file="/opt/ml/code/sd-dynamic-prompts/wildcards/colors.txt"
 
 # check local file exists
 if [ ! -f "$local_file" ]; then
     # Download the S3 file and save it to the local file
-    aws s3 cp "s3://$s3_bucket/$s3_object_key" "$local_file"
-    echo "S3 file has been downloaded to $local_file."
+    aws s3 cp "$s3_file" "$local_file"
+    echo "$s3_file has been downloaded to $local_file."
     return 0
 fi
 
@@ -30,12 +31,12 @@ local_etag=$(md5sum "$local_file" | awk '{print $1}')
 
 # Compare ETags to check if the files are the same
 if [ "$s3_etag" == "$local_etag" ]; then
-    echo "S3 file and local file are the same: $local_etag, No need to download."
+    echo "$s3_file and $local_file are the same: $local_etag, No need to download."
 else
-    echo "S3 file ETag: $s3_etag"
-    echo "Local file ETag: $local_etag"
-    aws s3 cp "s3://$s3_bucket/$s3_object_key" "$local_file"
-    echo "S3 file has been updated and downloaded to $local_file."
+    echo "$s3_file ETag: $s3_etag"
+    echo "$local_file ETag: $local_etag"
+    aws s3 cp "$s3_file" "$local_file"
+    echo "$s3_file has been updated and downloaded to $local_file."
 fi
 
 sleep $sleep_seconds
