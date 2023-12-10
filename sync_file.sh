9c531a05cf4c31cdf6c39d84c5e7b021b516784d
Jingyi <elonniu@amazon.com>
2023-12-10 23:40:16 +0800

    improved shell

--- sync_file.sh
+++ sync_file.sh
@@ -24,16 +24,16 @@ s3_metadata=$(aws s3api head-object --bucket "$s3_bucket" --key "$s3_object_key"
 
 # Extract the ETag (MD5 hash) from the S3 metadata without " (double quotes)
 s3_etag=$(echo "$s3_metadata" | jq -r '.ETag' | tr -d '"')
-echo "S3 ETag: $s3_etag"
 
 # Get the local file's MD5 hash
 local_etag=$(md5sum "$local_file" | awk '{print $1}')
-echo "Local ETag: $local_etag"
 
 # Compare ETags to check if the files are the same
 if [ "$s3_etag" == "$local_etag" ]; then
-    echo "S3 file and local file are the same. No need to download."
+    echo "S3 file and local file are the same: $local_etag, No need to download."
 else
+    echo "S3 file ETag: $s3_etag"
+    echo "Local file ETag: $local_etag"
     aws s3 cp "s3://$s3_bucket/$s3_object_key" "$local_file"
     echo "S3 file has been updated and downloaded to $local_file."
 fi
