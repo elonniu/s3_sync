8b6807e6a864a77e46f4b02d714b586828917aae
Jingyi <elonniu@amazon.com>
2023-12-11 21:08:47 +0800

    improved shell tip

--- sync_file.sh
+++ sync_file.sh
@@ -14,7 +14,7 @@ local_file="/opt/ml/code/extensions/sd-dynamic-prompts/wildcards/colors.txt"
 if [ ! -f "$local_file" ]; then
     # Download the S3 file and save it to the local file
     aws s3 cp "$s3_file" "$local_file"
-    echo "--- $s3_file has been downloaded to $local_file"
+    echo "$s3_file has been downloaded to $local_file"
     cat $local_file
     return 0
 fi
@@ -30,12 +30,12 @@ local_etag=$(md5sum "$local_file" | awk '{print $1}')
 
 # Compare ETags to check if the files are the same
 if [ "$s3_etag" == "$local_etag" ]; then
-    echo "--- $s3_file and $local_file are the same: $local_etag, No need to download."
+    echo "$s3_file and $local_file are the same: $local_etag, No need to download."
 else
     echo "$s3_file ETag: $s3_etag"
     echo "$local_file ETag: $local_etag"
     aws s3 cp "$s3_file" "$local_file"
-    echo "--- $s3_file has been updated and downloaded to $local_file"
+    echo "$s3_file has been updated and downloaded to $local_file"
 fi
 
 cat $local_file
